64a1280fb4a69863bd1a81387a091075c378f91d
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-05-05 12:57:37 -0500

    initial commit

---
+++ api/api.py
@@ -0,0 +1,19 @@
+import time
+import flask
+import psycopg2
+from flask import request, jsonify
+from psycopg2.extras import RealDictCursor
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+connection = psycopg2.connect(
+    host="127.0.0.1",
+    port="5432",
+    database="python_books"
+)
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
